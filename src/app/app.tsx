d79e333ced4bc5a7f3bed5c022c6f6a935eebf64
irojas <isaac568r@gmail.com>
2025-12-19 17:24:16 -0500

    move query-client to a separate module

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,8 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router';
+import { queryClient } from '../lib/tanstack-query/query-client';
 import { appRouter } from './app-router';
-
-const queryClient = new QueryClient();
 
 export const App = () => {
   return (
