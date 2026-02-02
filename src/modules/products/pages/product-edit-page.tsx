173d2e60577ad162c8f2f60972c5951519b55a36
irojas <isaac568r@gmail.com>
2026-02-02 16:31:22 -0500

    add type to useParams and update state messages in ProductEditPage

--- src/modules/products/pages/product-edit-page.tsx
+++ src/modules/products/pages/product-edit-page.tsx
@@ -7,26 +7,28 @@ import { ProductEdit } from '../components/product-edit';
 import { mapProductToEditForm } from '../mappers/product-to-edit-form.mapper';
 
 export const ProductEditPage = () => {
-  const params = useParams();
+  const { id } = useParams<{ id: string }>();
 
   const {
     data: product,
-    isLoading,
+    isPending,
     isError
   } = useQuery({
-    queryKey: ['productId', { id: params.id }],
-    queryFn: () => getProductByIdAction(params.id!),
+    queryKey: ['productId', { id: id }],
+    queryFn: () => getProductByIdAction(id!),
     staleTime: 1000 * 60 * 5,
     retry: false
   });
 
-  if (isLoading) return <h1>Cargando ... </h1>;
+  if (isPending) return <h1>Cargando ... </h1>;
 
-  if (!product || isError) return <h1>No se encontró el producto buscado</h1>;
+  if (isError) return <h1>Ocurrió un error inesperado... vuelva a intentar</h1>;
+
+  if (!product) return <h1>No se encontró el producto buscado</h1>;
 
   return (
     <div className="max-w-7xl mx-auto">
-      <Title title="Editar Producto" subtitle="Modifica tu producto." />
+      <Title title="Editar Producto" subtitle="Modifica aquí la información del producto." showLink={false} />
       <ProductEdit productForm={mapProductToEditForm(product)} productUI={product} />
     </div>
   );
