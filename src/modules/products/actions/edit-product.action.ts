34cbed546f83213ef966c6c6a1fa4e06b2611d50
irojas <isaac568r@gmail.com>
2026-02-17 18:24:37 -0500

    products: add query invalidation and api type

--- src/modules/products/actions/edit-product.action.ts
+++ src/modules/products/actions/edit-product.action.ts
@@ -2,7 +2,7 @@ import { editProduct } from '../api/products.api';
 import type { EditProductForm } from '../interfaces/ui/create-product-form.interface';
 import { mapProductToDisplay } from '../mappers/get-products-by-page.mapper';
 
-export const editProductAction = async ({ id, body }: { id: number; body: EditProductForm }) => {
+export const editProductAction = async ({ id, body }: { id: string; body: EditProductForm }) => {
   const updateProduct = await editProduct(id, body);
   return mapProductToDisplay(updateProduct);
 };
