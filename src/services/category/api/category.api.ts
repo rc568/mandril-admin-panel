55f71bc849cc325fd6edf5a5eec90f3db264c6f0
irojas <isaac568r@gmail.com>
2026-07-31 10:30:16 -0500

    category: refactor imports for consistency

--- src/services/category/api/category.api.ts
+++ src/services/category/api/category.api.ts
@@ -1,5 +1,5 @@
 import { baseApi } from '@/lib/axios/api';
-import type { GetCategoriesApiResponse } from './interfaces/get-all-categories.interface';
+import type { GetCategoriesApiResponse } from '../interfaces/api';
 
 export const getCategories = async () => {
   const response = await baseApi.get<GetCategoriesApiResponse>('/categories');
