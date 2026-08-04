1a5b2dea74418ce34da2999bad2ec8b716ceef4c
irojas <isaac568r@gmail.com>
2026-08-04 12:49:17 -0500

    eslint config: correct import usage for eslint-config-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
@@ -14,7 +15,7 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      'eslint-config-prettier'
+      eslintConfigPrettier
     ],
     languageOptions: {
       ecmaVersion: 2020,
